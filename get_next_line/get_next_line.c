0a7dbf87f380703f62491fa97201639df372373a
Roman <rmzvr@proton.me>
2024-11-05 22:27:36 +0100

    3

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,130 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rzvir <rzvir@student.42.fr>                +#+  +:+       +#+        */
+/*   By: rmzvr <rmzvr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 12:19:41 by rzvir             #+#    #+#             */
-/*   Updated: 2024/11/03 15:55:06 by rzvir            ###   ########.fr       */
+/*   Updated: 2024/11/05 21:52:01 by rmzvr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
+static int	ft_init_stash_and_buffer(char **buffer, char **stash)
 {
-	size_t	i;
-
-	i = 0;
-	while (s[i] != '\0')
+	if (*stash == NULL)
 	{
-		i++;
+		*stash = ft_strndup("", 1);
+		if (*stash == NULL)
+			return (1);
 	}
-	return (i);
+	*buffer = malloc(BUFFER_SIZE + 1);
+	if (*buffer == NULL)
+	{
+		free(*stash);
+		*stash = NULL;
+		return (1);
+	}
+	return (0);
 }
 
-char	*ft_strndup(const char *src, size_t n)
+static char	*ft_join_stash(ssize_t bytes_read, char **stash, char **buffer)
 {
-	char	*dest;
-	size_t	i;
+	char		*temp_stash;
 
-	dest = (char *)malloc(n + 1);
-	if (dest == NULL)
-		return (NULL);
-	i = 0;
-	while (i < n)
+	(*buffer)[bytes_read] = '\0';
+	temp_stash = ft_strjoin(*stash, *buffer);
+	free(*stash);
+	*stash = NULL;
+	if (temp_stash == NULL)
 	{
-		dest[i] = *src++;
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*joined_str;
-	size_t	i;
-	size_t	s1_len;
-
-	if (s1 == NULL || s1 == NULL)
-		return (NULL);
-	i = 0;
-	s1_len = ft_strlen(s1);
-	joined_str = malloc((s1_len + ft_strlen(s2)) + 1);
-	if (joined_str == NULL)
-	{
-		free(s1);
+		free(*buffer);
 		return (NULL);
 	}
-	while (i < (s1_len + ft_strlen(s2)))
-	{
-		if (i < s1_len)
-			joined_str[i] = s1[i];
-		else
-			joined_str[i] = s2[i - s1_len];
-		i++;
-	}
-	joined_str[i] = '\0';
-	free(s1);
-	return (joined_str);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	size_t	i;
-	size_t	src_len;
-
-	i = 0;
-	src_len = ft_strlen(src);
-	if (size == 0)
-	{
-		return (src_len);
-	}
-	while (src[i] != '\0' && i < size - 1 && size != 0)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (src_len);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	unsigned char	chr;
-	unsigned char	*str;
-
-	chr = (unsigned char) c;
-	str = (unsigned char *) s;
-	while (*str != '\0')
-	{
-		if (*str == chr)
-			return ((char *)str);
-		str++;
-	}
-	if (chr == '\0')
-	{
-		return ((char *)str);
-	}
-	return (NULL);
-}
-
-static int	ft_get_len_after_nl(char *str)
-{
-	int	count;
-
-	count = 0;
-	while (*str != '\n' && *str != '\0')
-		str++;
-	if (*str == '\0')
-		return (count);
-	str++;
-	while (*str != '\0')
-	{
-		str++;
-		count++;
-	}
-	return (count);
+	return (temp_stash);
 }
 
 static char	*ft_trim_stash(char *stash)
@@ -158,20 +75,18 @@ static char	*ft_trim_stash(char *stash)
 	return (copy);
 }
 
-// static void	ft_free(char **buffer, char **stash)
-// {
-// 	free(*buffer);
-// 	free(*stash);
-// 	*stash = NULL;
-// }
-
 static char	*ft_put_line(char **buffer, char **stash)
 {
 	char		*line;
-	int			chars_after_new_line;
+	size_t		len_to_nl;
+	char		*nl_ptr;
 
-	chars_after_new_line = ft_get_len_after_nl(*stash);
-	line = ft_strndup(*stash, ft_strlen(*stash) - chars_after_new_line);
+	nl_ptr = ft_strchr(*stash, '\n');
+	if (nl_ptr != NULL)
+		len_to_nl = nl_ptr - *stash + 1;
+	else
+		len_to_nl = ft_strlen(*stash);
+	line = ft_strndup(*stash, len_to_nl);
 	if (line == NULL)
 	{
 		free(*buffer);
@@ -184,83 +99,33 @@ static char	*ft_put_line(char **buffer, char **stash)
 	return (line);
 }
 
-// static int	ft_is_stash_or_buffer_invalid(char **buffer, char **stash)
-// {
-// 	if (*stash == NULL)
-// 	{
-// 		*stash = ft_strndup("", 1);
-// 		if (*stash == NULL)
-// 		{
-// 			return (1);
-// 		}
-// 	}
-// 	*buffer = malloc(BUFFER_SIZE + 1);
-// 	if (*buffer == NULL)
-// 	{
-// 		free(*stash);
-// 		*stash = NULL;
-// 		return (1);
-// 	}
-// 	return (0);
-// }
-
-static char	*ft_join_stash(ssize_t bytes_read, char **stash, char **buffer)
-{
-	if (bytes_read > 0)
-	{
-		(*buffer)[bytes_read] = '\0';
-		*stash = ft_strjoin(*stash, *buffer);
-		if (*stash == NULL)
-		{
-			free(*buffer);
-			return (NULL);
-		}
-	}
-	return (*stash);
-}
-
 char	*get_next_line(int fd)
 {
 	ssize_t		bytes_read;
-	char		*buffer;
 	static char	*stash;
-	int			done;
+	char		*buffer;
 
-	if (stash == NULL)
-	{
-		stash = ft_strndup("", 0);
-		if (stash == NULL)
-			return (NULL);
-	}
-	buffer = malloc(BUFFER_SIZE + 1);
-	if (buffer == NULL)
-	{
-		free(stash);
-		stash = NULL;
+	if (ft_init_stash_and_buffer(&buffer, &stash))
 		return (NULL);
-	}
-	done = 0;
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	while (!done)
+	while (bytes_read > 0)
 	{
 		stash = ft_join_stash(bytes_read, &stash, &buffer);
 		if (stash == NULL)
 			return (NULL);
-		if ((bytes_read > 0 && ft_strchr(buffer, '\n'))
-			|| (bytes_read == 0 && ft_strlen(stash) > 0))
+		if (ft_strchr(stash, '\n'))
 			return (ft_put_line(&buffer, &stash));
-		if (bytes_read > 0)
-			bytes_read = read(fd, buffer, BUFFER_SIZE);
-		else
-			done = 1;
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
 	}
+	if (bytes_read == 0 && ft_strlen(stash) > 0)
+		return (ft_put_line(&buffer, &stash));
 	free(buffer);
 	free(stash);
 	stash = NULL;
 	return (NULL);
 }
 
-/* int	main(void)
+int	ft_run(void)
 {
 	int		fd;
 	char	*p;
@@ -275,4 +140,22 @@ char	*get_next_line(int fd)
 	}
 	printf("%s", p);
 	return (0);
-} */
+}
+
+// int main() {
+//     // Start time
+//     clock_t start_time = clock();
+
+//     // Code to measure
+// 	ft_run();
+
+//     // End time
+//     clock_t end_time = clock();
+
+//     // Calculate the elapsed time in milliseconds
+//     double time_taken = ((double)(end_time - start_time) / CLOCKS_PER_SEC) * 1000.0;
+
+//     printf("\nExecution time: %.3f ms\n", time_taken);
+
+//     return 0;
+// }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rzvir <rzvir@student.42.fr>                +#+  +:+       +#+        */
+/*   By: rmzvr <rmzvr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 12:21:05 by rzvir             #+#    #+#             */
-/*   Updated: 2024/11/02 17:58:13 by rzvir            ###   ########.fr       */
+/*   Updated: 2024/11/05 21:30:41 by rmzvr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 10
 # endif
 
 // read
@@ -25,12 +25,14 @@
 # include <fcntl.h>
 //! printf
 # include <stdio.h>
+//! time
+# include <time.h>
 
 char	*get_next_line(int fd);
-// char	*ft_strjoin(char *s1, char *s2);
-// size_t	ft_strlen(const char *s);
-// char	*ft_strchr(const char *s, int c);
-// char	*ft_strndup(const char *src, size_t n);
-// size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strndup(const char *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
