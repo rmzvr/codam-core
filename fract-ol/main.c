bb0981ce12554efeebd7c007b390391944cc3fdc
rmzvr <rmzvr@proton.me>
2024-11-25 17:51:13 +0100

    fract-ol

--- fract-ol/main.c
+++ fract-ol/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzvir <rzvir@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 14:11:45 by rzvir             #+#    #+#             */
+/*   Updated: 2024/11/25 17:47:57 by rzvir            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "mlx.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,10 +41,11 @@ typedef struct	s_shift
 typedef struct	s_mlx {
 	void		*ptr;
 	void		*win_ptr;
-	t_img		img1;
-	t_img		img2;
+	t_img		img;
 	int			iterations;
+	int			max_iterations;
 	t_complex	z;
+	t_complex	zd;
 	t_complex	c;
 	int			window_width;
 	int			window_height;
@@ -51,29 +64,6 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 	offset = (y * img->line_length + x * (img->bits_per_pixel / 8));
 	curr_pixel = (unsigned int *)(img->pixels_addr + offset);
 	*curr_pixel = color;
-}
-
-int	encode_rgb(unsigned char red, unsigned char green, unsigned char blue)
-{
-	return (red << 16 | green << 8 | blue);
-}
-
-int	handle_close(t_mlx *mlx)
-{
-	// mlx_destroy_image(mlx->ptr, mlx->img.ptr);
-	mlx_destroy_window(mlx->ptr, mlx->win_ptr);
-	mlx_destroy_display(mlx->ptr);
-	free(mlx->ptr);
-	exit(1);
-	return (1);
-}
-
-int	handle_zoom(int x, int y, t_mlx *mlx)
-{
-	(void) mlx;
-	printf("x: %d", x);
-	printf("y: %d", y);
-	return (1);
 }
 
 double	scale_x(int i, int width)
@@ -96,126 +86,100 @@ double	scale_y(int i, int height)
 	return (1.5 - -1.5) * (i - 0.0) / (d_height - 0.0) + -1.5;
 }
 
+int	encode_rgb(unsigned char red, unsigned char green, unsigned char blue)
+{
+	return (red << 16 | green << 8 | blue);
+}
+
 void	render_fractal(t_mlx *mlx, double zoom, t_shift shift, t_img *img)
 {
-	(void) shift;
-	(void) zoom;
-	mlx->x = 0;
-	mlx->y = 0;
-	mlx->iterations = 0;
-	mlx->z.real = 0;
-	mlx->z.imaginary = 0;
-	mlx->c.real = (scale_x(mlx->x, mlx->window_width));
-	mlx->c.imaginary = (scale_y(mlx->y, mlx->window_height));
 	while (mlx->y < mlx->window_height)
 	{
 		mlx->x = 0;
+		mlx->c.imaginary = (scale_y(mlx->y + shift.bottom, mlx->window_height)) * zoom;
 		while (mlx->x < mlx->window_width)
 		{
 			mlx->iterations = 0;
-			while (mlx->iterations < 1000)
+			mlx->z.real = 0;
+			mlx->z.imaginary = 0;
+			mlx->zd.real = 0;
+			mlx->zd.imaginary = 0;
+			mlx->c.real = (scale_x(mlx->x + shift.right, mlx->window_width)) * zoom;
+			while (mlx->iterations < mlx->max_iterations)
 			{
-				if (mlx->z.real < -2 || mlx->z.real > 1 || mlx->z.imaginary < -1.5 || mlx->z.imaginary > 1.5)
-				{
-					my_mlx_pixel_put(img, mlx->x, mlx->y, (unsigned int)0x000000 + (unsigned int)mlx->iterations + 40);
-					mlx->z.real = 0;
-					mlx->z.imaginary = 0;
-					break;
-				}
-				else
-				{
-					my_mlx_pixel_put(img, mlx->x, mlx->y, 0xFFFFFF);
-				}
+
+				double temp_zd_real = 2 * (mlx->z.real * mlx->zd.real - mlx->z.imaginary * mlx->zd.imaginary) + 1;
+				mlx->zd.imaginary = 2 * (mlx->z.real * mlx->zd.imaginary + mlx->z.imaginary * mlx->zd.real);
+				mlx->zd.real = temp_zd_real;
+
 				double temp_real = (mlx->z.real * mlx->z.real) - (mlx->z.imaginary * mlx->z.imaginary);
 				mlx->z.imaginary = 2 * mlx->z.real * mlx->z.imaginary;
 				mlx->z.real = temp_real;
-
 				mlx->z.real += mlx->c.real;
 				mlx->z.imaginary += mlx->c.imaginary;
-				mlx->iterations++;
-				double res = sqrt((mlx->z.real * mlx->z.real) + (mlx->z.imaginary * mlx->z.imaginary));
+				int	res = (mlx->z.real * mlx->z.real) + (mlx->z.imaginary * mlx->z.imaginary);
 				if (res > 4)
 				{
-						my_mlx_pixel_put(img, mlx->x, mlx->y, (unsigned int)0x000000 + (unsigned int)mlx->iterations + 40);
-					mlx->z.real = 0;
-					mlx->z.imaginary = 0;
+					double	d = (res * log(res)) / sqrt((mlx->zd.real * mlx->zd.real) + (mlx->zd.imaginary * mlx->zd.imaginary));
+					if (d < 0.0001)
+						my_mlx_pixel_put(img, mlx->x, mlx->y, encode_rgb(0, 0, 0));
+					else
+						my_mlx_pixel_put(img, mlx->x, mlx->y, encode_rgb(255, 255, 255));
+					// my_mlx_pixel_put(img, mlx->x, mlx->y, encode_rgb(255, 255, 255));
 					break;
-				}
-				else
-				{
-					my_mlx_pixel_put(img, mlx->x, mlx->y, 0xFFFFFF);
 				}
 				mlx->iterations++;
 			}
+			if (mlx->iterations == mlx->max_iterations)
+				my_mlx_pixel_put(img, mlx->x, mlx->y, encode_rgb(0, 0, 0));
 			mlx->x++;
-			mlx->c.real = (scale_x(mlx->x + shift.right, mlx->window_width)) * zoom;
 		}
 		mlx->y++;
-		mlx->c.imaginary = (scale_y(mlx->y + shift.bottom, mlx->window_height)) * zoom;
 	}
-}
-void	render_double_buffering(t_mlx *mlx)
-{
-	t_img	*curr_img;
-
-	if (mlx->current_buffer == 0)
-	{
-		curr_img = &mlx->img1;
-	}
-	else
-	{
-		curr_img = &mlx->img2;
-	}
-
-	printf("curr_img %p\n", curr_img->ptr);
-
-	render_fractal(mlx, mlx->zoom, mlx->shift, curr_img);
-
-	mlx_put_image_to_window(mlx->ptr, mlx->win_ptr, curr_img->ptr, 0, 0);
-
-	mlx->current_buffer = 1 - mlx->current_buffer;
 }
 
 int	handle_input(int keysym, t_mlx *mlx)
 {
-	// if (keysym == XK_Escape)
-	// {
-	// 	mlx_destroy_image(mlx->ptr, mlx->img2.ptr);
-	// 	mlx->img2.ptr = mlx_new_image(mlx->ptr, mlx->window_width, mlx->window_height);
-	// 	my_mlx_pixel_put(&mlx->img2, mlx->window_width / 2 - 100, mlx->window_height / 2 - 100, 0xFFFFFF);
-	// 	mlx_put_image_to_window(mlx->ptr, mlx->win_ptr, mlx->img2.ptr, 0, 0);
-	// 	// mlx_destroy_window(mlx->ptr, mlx->win_ptr);
-	// 	// mlx_destroy_display(mlx->ptr);
-	// 	// free(mlx->ptr);
-	// 	// exit(1);
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_image(mlx->ptr, mlx->img.ptr);
+		mlx_destroy_window(mlx->ptr, mlx->win_ptr);
+		mlx_destroy_display(mlx->ptr);
+		free(mlx->ptr);
+		exit(1);
+	}
 
 	if (keysym == XK_Up)
 	{
 		printf("XK_Up\n");
-		mlx->shift.bottom -= 200 * mlx->zoom;
+		mlx->shift.bottom -= 200;
 		printf("bottom = %d\n", mlx->shift.bottom);
-		render_double_buffering(mlx);
+		render_fractal(mlx, mlx->zoom, mlx->shift, &mlx->img);
+		mlx_put_image_to_window(mlx->ptr, mlx->win_ptr, mlx->img.ptr, 0, 0);
 	}
 	else if (keysym == XK_Down)
 	{
 		printf("XK_Down\n");
-		mlx->shift.bottom += 200 * mlx->zoom;
+		mlx->shift.bottom += 200;
 		printf("bottom = %d\n", mlx->shift.bottom);
-		render_double_buffering(mlx);
+		render_fractal(mlx, mlx->zoom, mlx->shift, &mlx->img);
+		mlx_put_image_to_window(mlx->ptr, mlx->win_ptr, mlx->img.ptr, 0, 0);
 	}
 	else if (keysym == XK_Left)
 	{
 		printf("XK_Left\n");
-		mlx->shift.right -= 200 * mlx->zoom;
+		mlx->shift.right -= 200;
 		printf("right = %d\n", mlx->shift.right);
-		render_double_buffering(mlx);
+		render_fractal(mlx, mlx->zoom, mlx->shift, &mlx->img);
+		mlx_put_image_to_window(mlx->ptr, mlx->win_ptr, mlx->img.ptr, 0, 0);
 	}
 	else if (keysym == XK_Right)
 	{
 		printf("XK_Right\n");
-		mlx->shift.right += 200 * mlx->zoom;
+		mlx->shift.right += 200;
 		printf("right = %d\n", mlx->shift.right);
-		render_double_buffering(mlx);
+		render_fractal(mlx, mlx->zoom, mlx->shift, &mlx->img);
+		mlx_put_image_to_window(mlx->ptr, mlx->win_ptr, mlx->img.ptr, 0, 0);
 	}
 	return (0);
 }
@@ -224,7 +188,6 @@ int mouse_hook(int button, int x, int y, t_mlx *mlx)
 {
 	(void) x;
 	(void) y;
-	printf("button %d\n", button);
 
 	if (button == 4)
 	{
@@ -234,13 +197,15 @@ int mouse_hook(int button, int x, int y, t_mlx *mlx)
 		else
 			mlx->zoom -= 0.1;
 		printf("zoom = %f\n", mlx->zoom);
-		render_double_buffering(mlx);
+		render_fractal(mlx, mlx->zoom, mlx->shift, &mlx->img);
+		mlx_put_image_to_window(mlx->ptr, mlx->win_ptr, mlx->img.ptr, 0, 0);
 	}
 	else if (button == 5)
 	{
 		printf("XK_minus\n");
 		mlx->zoom += 0.1;
-		render_double_buffering(mlx);
+		render_fractal(mlx, mlx->zoom, mlx->shift, &mlx->img);
+		mlx_put_image_to_window(mlx->ptr, mlx->win_ptr, mlx->img.ptr, 0, 0);
 	}
 	return (1);
 }
@@ -248,19 +213,23 @@ int mouse_hook(int button, int x, int y, t_mlx *mlx)
 int	main(void)
 {
 	t_mlx	mlx;
-
+	mlx.x = 0;
+	mlx.y = 0;
+	mlx.zoom = 1.0;
 	mlx.shift.top = 0;
 	mlx.shift.left = 0;
 	mlx.shift.right = 0;
 	mlx.shift.bottom = 0;
-	mlx.zoom = 1.0;
-	mlx.x = 0;
-	mlx.y = 0;
-	mlx.window_width = 800;
-	mlx.window_height = 800;
+	mlx.window_width = 1920;
+	mlx.window_height = 1920;
+	mlx.max_iterations = 6;
+
+	//! Initiate mlx library
 	mlx.ptr = mlx_init();
 	if (mlx.ptr == NULL)
 		return (1);
+
+	//! Create new window
 	mlx.win_ptr = mlx_new_window(mlx.ptr, mlx.window_width, mlx.window_height, "Hello world!");
 	if (mlx.win_ptr == NULL)
 	{
@@ -269,74 +238,39 @@ int	main(void)
 		return (1);
 	}
 
-	// img1
-	mlx.img1.ptr = mlx_new_image(mlx.ptr, mlx.window_width, mlx.window_height);
-	if (mlx.img1.ptr == NULL)
+	//! Create new image
+	mlx.img.ptr = mlx_new_image(mlx.ptr, mlx.window_width, mlx.window_height);
+	if (mlx.img.ptr == NULL)
 	{
 		mlx_destroy_window(mlx.ptr, mlx.win_ptr);
 		mlx_destroy_display(mlx.ptr);
 		free(mlx.ptr);
 		exit(1);
 	}
-	mlx.img1.pixels_addr = mlx_get_data_addr(mlx.img1.ptr, &mlx.img1.bits_per_pixel, &mlx.img1.line_length, &mlx.img1.endian);
-	if (mlx.img1.pixels_addr == NULL)
+	//! Get image pointer address
+	mlx.img.pixels_addr = mlx_get_data_addr(mlx.img.ptr, &mlx.img.bits_per_pixel, &mlx.img.line_length, &mlx.img.endian);
+	if (mlx.img.pixels_addr == NULL)
 	{
-		mlx_destroy_image(mlx.ptr, mlx.img1.ptr);
+		mlx_destroy_image(mlx.ptr, mlx.img.ptr);
 		mlx_destroy_window(mlx.ptr, mlx.win_ptr);
 		mlx_destroy_display(mlx.ptr);
 		free(mlx.ptr);
 		exit(1);
 	}
 
-	// img2
-		mlx.img2.ptr = mlx_new_image(mlx.ptr, mlx.window_width, mlx.window_height);
-	if (mlx.img2.ptr == NULL)
-	{
-		mlx_destroy_window(mlx.ptr, mlx.win_ptr);
-		mlx_destroy_display(mlx.ptr);
-		free(mlx.ptr);
-		exit(1);
-	}
-	mlx.img2.pixels_addr = mlx_get_data_addr(mlx.img2.ptr, &mlx.img2.bits_per_pixel, &mlx.img2.line_length, &mlx.img2.endian);
-	if (mlx.img2.pixels_addr == NULL)
-	{
-		mlx_destroy_image(mlx.ptr, mlx.img2.ptr);
-		mlx_destroy_window(mlx.ptr, mlx.win_ptr);
-		mlx_destroy_display(mlx.ptr);
-		free(mlx.ptr);
-		exit(1);
-	}
-	
-	mlx.current_buffer = 0;
+	//! Render fractal
+	render_fractal(&mlx, mlx.zoom, mlx.shift, &mlx.img);
+	mlx_put_image_to_window(mlx.ptr, mlx.win_ptr, mlx.img.ptr, 0, 0);
 
-	render_double_buffering(&mlx);
-
-	// mlx_put_image_to_window(mlx.ptr, mlx.win_ptr, mlx.img.ptr, 0, 0);
-	// printf("before img2\n");
-	// mlx.img2.ptr = mlx_new_image(mlx.ptr, mlx.window_width, mlx.window_height);
-	// if (mlx.img2.ptr == NULL)
-	// {
-	// 	mlx_destroy_window(mlx.ptr, mlx.win_ptr);
-	// 	mlx_destroy_display(mlx.ptr);
-	// 	free(mlx.ptr);
-	// 	exit(1);
-	// }
-	// mlx.img2.pixels_addr = mlx_get_data_addr(mlx.img2.ptr, &mlx.img2.bits_per_pixel, &mlx.img2.line_length, &mlx.img2.endian);
-	// if (mlx.img2.pixels_addr == NULL)XK_Right
-	// {
-	// 	mlx_destroy_image(mlx.ptr, mlx.img2.ptr);
-	// 	mlx_destroy_window(mlx.ptr, mlx.win_ptr);
-	// 	mlx_destroy_display(mlx.ptr);
-	// 	free(mlx.ptr);
-	// 	exit(1);
-	// }
-	// my_mlx_pixel_put(&mlx.img2, mlx.window_width / 2, mlx.window_height / 2, 0xFFFFFF);
-	// mlx_put_image_to_window(mlx.ptr, mlx.win_ptr, mlx.img2.ptr, 0, 0);
+	//! Graphic management
 	mlx_key_hook(mlx.win_ptr, handle_input, &mlx);
 	mlx_mouse_hook(mlx.win_ptr, mouse_hook, &mlx);
-	mlx_hook(mlx.win_ptr, 17, 0L, handle_close, &mlx);
+
+	//! Keep running
 	mlx_loop(mlx.ptr);
-	// mlx_destroy_image(mlx.ptr, mlx.img.ptr);
+
+	//! Clean up
+	mlx_destroy_image(mlx.ptr, mlx.img.ptr);
 	mlx_destroy_window(mlx.ptr, mlx.win_ptr);
 	mlx_destroy_display(mlx.ptr);
 	free(mlx.ptr);
