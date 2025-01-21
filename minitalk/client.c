46536d688986ec8a4fa2d0c03f79fac3c24dfed9
rmzvr <rmzvr@proton.me>
2025-01-21 17:19:13 +0100

    minitalk 1

--- minitalk/client.c
+++ minitalk/client.c
@@ -3,31 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmzvr <rmzvr@student.42.fr>                +#+  +:+       +#+        */
+/*   By: rzvir <rzvir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 10:54:53 by rmzvr             #+#    #+#             */
-/*   Updated: 2025/01/20 23:41:58 by rmzvr            ###   ########.fr       */
+/*   Updated: 2025/01/21 17:17:53 by rzvir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
-#include <aio.h>
-#include <signal.h>
+#include "main.h"
+
+static volatile int	g_signal_pid = 1;
+
+void	get_pid(int signal_number)
+{
+	(void) signal_number;
+	g_signal_pid = 0;
+	ft_putnbr_fd(g_signal_pid, 1);
+	// write(1, "0", 1);
+	return ;
+}
 
 int	main(void)
 {
-	int		i;
-	int		mask;
-	char	*message;
-	char	character;
-	char	iteration;
-	pid_t	process_id;
+	int			i;
+	int			mask;
+	char		*message;
+	char		character;
+	char		iteration;
+	sigset_t	signal_set;
+	t_action	signal_action;
 
+	signal_action.sa_flags = 0;
+	signal_action.sa_handler = get_pid;
+	sigaddset(&signal_set, SIGUSR1);
+	sigaddset(&signal_set, SIGUSR2);
+	sigaction(SIGUSR1, &signal_action, NULL);
 	i = 0;
 	mask = 128;
-	message = ft_strdup("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales lacus et tellus mollis, in interdum arcu accumsan. Vivamus sit amet rutrum ipsum. Proin bibendum finibus congue. Maecenas diam ex, porta quis congue vel, auctor porttitor justo. Quisque congue ligula libero, non placerat metus pharetra sed. Cras lacinia tortor a purus interdum sagittis. Mauris a magna nisl. Suspendisse quis nulla ac lectus vulputate malesuada id vitae dui. Nam ac sem faucibus, interdum nunc at, dapibus ex. Vestibulum mattis cursus mollis. Ut rhoncus sollicitudin gravida.\n");
-	process_id = getpid();
-	ft_printf("The client ID is %d\n", process_id);
+	message = ft_strdup("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+	// ft_printf("The client ID is %d\n", process_id);
+	kill(140033, SIGUSR1);
+	while (g_signal_pid)
+		usleep(42);
+	usleep(42);
+	g_signal_pid = 0;
+	// ft_printf("Server's ID on client side: %d\n", g_signal_pid);
 	while (message[i] != '\0')
 	{
 		iteration = 0;
@@ -36,17 +56,24 @@ int	main(void)
 		{
 			if (character & mask)
 			{
-				kill(550716, SIGUSR2);
+				kill(140033, SIGUSR2);
 			}
 			else
-			{
-				kill(550716, SIGUSR1);
-			}
-			usleep(2000);
+				kill(140033, SIGUSR1);
 			iteration++;
 			character <<= 1;
+			g_signal_pid = 1;
+			while (g_signal_pid)
+				usleep(42);
+			usleep(42);
+			g_signal_pid = 0;
+			write(1, "1", 1);
+			// ft_printf("Server's ID on client side: %d\n", g_signal_pid);
 		}
+		iteration = 0;
 		i++;
 	}
+	free(message);
+	message = NULL;
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -3,29 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmzvr <rmzvr@student.42.fr>                +#+  +:+       +#+        */
+/*   By: rzvir <rzvir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 10:54:51 by rmzvr             #+#    #+#             */
-/*   Updated: 2025/01/20 23:40:37 by rmzvr            ###   ########.fr       */
+/*   Updated: 2025/01/21 17:17:48 by rzvir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
-#include <unistd.h>
-#include <stdio.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <stdio.h>
+#include "main.h"
 
-volatile sig_atomic_t g_sigusr = 0;
+static volatile sig_atomic_t	g_signal = -1;
+static volatile int	g_pause = 1;
 
-typedef struct s_sig
+void	get_pid(int signal_number, siginfo_t *info, void *context)
 {
-	int	character;
-	int	bits;
-}	t_sig;
+	(void) context;
+	if (g_signal == -1)
+		g_signal = info->si_pid;
+	else
+		g_signal = signal_number;
 
-void	handle_sigusr(int signum)
+	g_pause = 0;
+	// ft_putnbr_fd(g_pause, 1);
+	// ft_putnbr_fd(g_pause, 1);
+	// write(1, "0", 1);
+}
+
+void	handle_sig(int signal_number)
 {
 	static t_sig	sig;
 
@@ -33,45 +37,53 @@ void	handle_sigusr(int signum)
 		sig.character = 0;
 	if (!sig.bits)
 		sig.bits = 1;
-	if (signum == 10)
+	if (signal_number == 10)
 		sig.character ^= 0;
-	else if (signum == 12)
+	else if (signal_number == 12)
 		sig.character ^= 1;
 	if (sig.bits == 8)
 	{
-		// ft_putnbr_fd(sig.bits, 1);
 		write(1, &sig.character, 1);
 		sig.bits = 1;
 		sig.character = 0;
 		return ;
 	}
-	// if (sig.bits > 8)
-	// {
-	// 	ft_putnbr_fd(sig.bits, 1);
-
-	// }
 	sig.bits++;
 	sig.character <<= 1;
 }
 
-typedef struct sigaction action_t;
-
 int	main(void)
 {
-	pid_t		pid;
+	pid_t		process_id;
 	sigset_t	signal_set;
-	action_t	SIGUSR_action;
+	t_action	signal_action;
+	int			sender_process_id;
 
-	SIGUSR_action.sa_handler = handle_sigusr;
+	ft_bzero(&signal_action, sizeof(signal_action));
+	signal_action.sa_flags = SA_SIGINFO;
+	signal_action.sa_sigaction = get_pid;
 	sigemptyset(&signal_set);
-	SIGUSR_action.sa_flags = 0;
-
-	sigaction(SIGUSR1, &SIGUSR_action, NULL);
-	sigaction(SIGUSR2, &SIGUSR_action, NULL);
-
-	pid = getpid();
-	ft_printf("The server ID is %d\n", pid);
+	sigaction(SIGUSR1, &signal_action, NULL);
+	sigaction(SIGUSR2, &signal_action, NULL);
+	process_id = getpid();
+	ft_printf("The server ID is %d\n", process_id);
 	while (1)
-		pause();
+	{
+		g_pause = 1;
+		while (g_pause)
+			usleep(42);
+		usleep(42);
+		g_pause = 0;
+		// write(1, "1", 1);
+		if (g_signal > 31)
+		{
+			sender_process_id = g_signal;
+			// ft_printf("Client's ID on server side: %d\n", g_signal);
+		}
+		else
+			handle_sig(g_signal);
+		g_signal = -2;
+		kill(sender_process_id, SIGUSR1);
+	}
 	return (0);
 }
