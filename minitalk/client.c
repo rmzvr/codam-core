ca9bc12ec796ed8bcda6001fa403205636bf7101
rmzvr <rmzvr@proton.me>
2025-01-22 12:12:21 +0100

    final minitalk

--- minitalk/client.c
+++ minitalk/client.c
@@ -3,57 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmzvr <rmzvr@student.1000.fr>                +#+  +:+       +#+        */
+/*   By: rzvir <rzvir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 10:54:53 by rmzvr             #+#    #+#             */
-/*   Updated: 2025/01/21 18:05:01 by rmzvr            ###   ########.fr       */
+/*   Updated: 2025/01/22 12:10:13 by rzvir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "main.h"
+#include "libft/libft.h"
+#include <signal.h>
 
-void	get_sig(int signal_number)
+static volatile sig_atomic_t	g_pause = 1;
+
+static void	handle_signal(int signal_number)
 {
 	(void) signal_number;
+	g_pause = 0;
 }
 
-int	main(int argc, char **argv)
+void	set_signal_action(void)
 {
-	(void)		argc;
-	int			i;
-	int			mask;
-	char		*message;
-	char		character;
-	char		iteration;
-	sigset_t	signal_set;
-	t_action	signal_action;
+	sigset_t			signal_set;
+	struct sigaction	signal_action;
 
-	ft_printf("__PID: %d\n", getpid());
+	ft_bzero(&signal_action, sizeof(signal_action));
+	signal_action.sa_handler = handle_signal;
 	signal_action.sa_flags = 0;
-	signal_action.sa_handler = get_sig;
 	sigemptyset(&signal_set);
 	sigaction(SIGUSR1, &signal_action, NULL);
+}
+
+static void	send_message(int server_pid, char *message)
+{
+	int		i;
+	char	character;
+	char	iteration;
+
 	i = 0;
-	mask = 0b10000000;
-	message = ft_strdup(argv[2]);
 	while (message[i] != '\0')
 	{
 		iteration = 0;
 		character = message[i];
 		while (iteration < 8)
 		{
-			if (character & mask)
-				kill(ft_atoi(argv[1]), SIGUSR2);
+			if (character & 0b10000000)
+				kill(server_pid, SIGUSR2);
 			else
-				kill(ft_atoi(argv[1]), SIGUSR1);
-			pause();
+				kill(server_pid, SIGUSR1);
 			iteration++;
 			character <<= 1;
+			while (g_pause)
+				usleep(50);
+			g_pause = 1;
 		}
 		iteration = 0;
 		i++;
 	}
-	free(message);
-	message = NULL;
+}
+
+int	main(int argc, char **argv)
+{
+	char	*message;
+	int		server_pid;
+
+	(void) argc;
+	message = argv[2];
+	server_pid = ft_atoi(argv[1]);
+	set_signal_action();
+	send_message(server_pid, message);
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -3,68 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rzvir <rzvir@student.100.fr>                +#+  +:+       +#+        */
+/*   By: rzvir <rzvir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 10:54:51 by rmzvr             #+#    #+#             */
-/*   Updated: 2025/01/21 17:17:48 by rzvir            ###   ########.fr       */
+/*   Updated: 2025/01/22 12:10:12 by rzvir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "main.h"
+#include "libft/libft.h"
+#include <signal.h>
 
-void	handle_sig(int signal_number)
+static void	decode_bits(int signal_number)
 {
-	static t_sig	sig;
+	static int	character;
+	static int	current_bit;
 
-	if (!sig.character)
-		sig.character = 0;
-	if (!sig.bits)
-		sig.bits = 1;
+	if (!character)
+		character = 0;
+	if (!current_bit)
+		current_bit = 1;
 	if (signal_number == 10)
-		sig.character ^= 0;
+		character ^= 0;
 	else if (signal_number == 12)
-		sig.character ^= 1;
-	if (sig.bits == 8)
+		character ^= 1;
+	if (current_bit == 8)
 	{
-		write(1, &sig.character, 1);
-		sig.bits = 1;
-		sig.character = 0;
+		ft_putchar_fd(character, 1);
+		current_bit = 1;
+		character = 0;
 		return ;
 	}
-	sig.bits++;
-	sig.character <<= 1;
+	current_bit++;
+	character <<= 1;
 }
 
-void	get_pid(int signal_number, siginfo_t *info, void *context)
+static void	handle_signal(int signal_number, siginfo_t *info, void *context)
 {
 	(void) context;
-	handle_sig(signal_number);
-	usleep(100);
+	decode_bits(signal_number);
 	kill(info->si_pid, SIGUSR1);
 }
 
-// ! DELETE
-void close_(int signum){
-	(void)signum;
-	exit(0);
+static void	set_signal_action(void)
+{
+	sigset_t			signal_set;
+	struct sigaction	signal_action;
+
+	ft_bzero(&signal_action, sizeof(signal_action));
+	signal_action.sa_sigaction = handle_signal;
+	signal_action.sa_flags = SA_SIGINFO;
+	sigemptyset(&signal_set);
+	sigaction(SIGUSR1, &signal_action, NULL);
+	sigaction(SIGUSR2, &signal_action, NULL);
 }
 
 int	main(void)
 {
-	pid_t		process_id;
-	sigset_t	signal_set;
-	t_action	signal_action;
+	pid_t	server_pid;
 
-	// ! DELETE
-	signal(SIGINT, &close_);
-	ft_bzero(&signal_action, sizeof(signal_action));
-	signal_action.sa_flags = SA_SIGINFO;
-	signal_action.sa_sigaction = get_pid;
-	sigemptyset(&signal_set);
-	sigaction(SIGUSR1, &signal_action, NULL);
-	sigaction(SIGUSR2, &signal_action, NULL);
-	process_id = getpid();
-	ft_printf("The server ID is %d\n", process_id);
+	set_signal_action();
+	server_pid = getpid();
+	ft_printf("The server ID is %d\n", server_pid);
 	while (1)
 		pause();
 	return (0);
